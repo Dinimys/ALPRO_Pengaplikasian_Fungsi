3eac7bb39e4b49bde53044a67f65a4d81d42a061
Laurensia Dini <114977501+Dinimys@users.noreply.github.com>
2022-10-16 22:37:01 +0800

    Add files via upload

---
+++ Keliling Persegi Panjang Fungsi.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+void cover(){
+    printf("\t\t\t--------------------------------------------------------------\n");
+    printf("\t\t\t|----------------PROGRAM BANGUN RUANG KUBUS------------------|\n");
+    printf("\t\t\t|============================================================|\n");
+    printf("\t\t\t|>> Laurensia Dini Marys Haryanto   ||     2205551028        |\n");
+    printf("\t\t\t--------------------------------------------------------------\n");
+}
+
+int k_persegi_panjang(float a, float b){
+    float keliling;
+
+    // perhitungan keliling persegi panjang
+    keliling = (2*a)+(2*b);
+    printf("keliling persegi panjang : %.f\n",keliling);
+    return keliling;
+}
+
+int l_persegi_panjang(float a, float b){
+    float luas;
+
+    // perhitungan luas persegi panjang
+    luas = (a*b);
+    printf ("luas persegi panjang : %.f\n",luas);
+    return luas;
+}
+
+int main ()
+{
+    // Deklarasi variabel
+    float panjang,lebar,keliling, luas ;
+
+    printf ("panjang persegi : ");
+    scanf ("%f", &panjang);
+
+    printf ("lebar persegi : ");
+    scanf (" %f", &lebar);
+
+    k_persegi_panjang(panjang, lebar);
+    l_persegi_panjang(panjang, lebar);
+
+return 0;
+}
+
